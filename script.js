5dabf06e1de36bd4bf3ac289bbe331efc90f7db4
Michelle <mmmorttey@gmail.com>
2025-07-23 14:30:44 -0400

    trying to add function to math button

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+let rFactsM= ['fact1', 'fact2', 'fact3', 'fact4'];
+let MathB = document.querySelector('.button.math');
 
+function getRMathFact() {
+    let randomIndex = Math.floor(Math.random() * rFactsM.length);
+    let randomFact = rFactsM[randomIndex];
+    alert(randomFact);
+}
+
+MathB.addEventListener('click', getRMathFact);
 
